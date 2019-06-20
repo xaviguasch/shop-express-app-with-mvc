f008a5a16111eadbd1c9ac58ab3470813995485c
xaviguasch <xaviguasch@gmail.com>
2019-06-20 10:57:17 +0200

    finished the controllers

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const path = require('path')
 const express = require('express')
 
 const productsController = require('../controllers/products')
+
 const router = express.Router()
 
 // /admin/add-product => GET
