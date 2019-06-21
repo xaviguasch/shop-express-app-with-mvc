f795fe754f9d761eee68d3ab4ca8bc52f249ddfb
xaviguasch <xaviguasch@gmail.com>
2019-06-21 16:22:05 +0200

    extracted product data from the forms

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,5 @@
 const Product = require('../models/product')
 
-exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title)
-  product.save()
-  res.redirect('/')
-}
-
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Product',
@@ -14,6 +8,16 @@ exports.getAddProduct = (req, res, next) => {
     productCSS: true,
     activeAddProduct: true
   })
+}
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title
+  const imageUrl = req.body.imageUrl
+  const price = req.body.price
+  const description = req.body.description
+  const product = new Product(title, imageUrl, price, description)
+  product.save()
+  res.redirect('/')
 }
 
 exports.getProducts = (req, res, next) => {
