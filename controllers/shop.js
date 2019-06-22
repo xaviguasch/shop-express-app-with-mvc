b523c1607e32d4df3cc9bdc1d33c8d06e2c02c18
xaviguasch <xaviguasch@gmail.com>
2019-06-22 11:49:56 +0200

    passed data with POST requests

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,10 +13,18 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId
   Product.findById(prodId, product => {
-    console.log(product)
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    })
   })
+}
 
-  res.redirect('/')
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId
+  console.log(prodId)
+  res.redirect('/cart')
 }
 
 exports.getIndex = (req, res, next) => {
